9ac0c486f69e909fbe1eb65a8b6d776b61d670f7
mpgxc <mpgx5.c@gmail.com>
2020-08-29 14:44:35 -0300

    feat: connect typeorm on models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,24 @@
+import {
+    Entity,
+    Column,
+    UpdateDateColumn,
+    CreateDateColumn,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('category')
 class Category {
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-  title: string;
+    @Column()
+    title: string;
 
-  created_at: Date;
+    @CreateDateColumn()
+    created_at: Date;
 
-  updated_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Category;
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,40 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transsactions')
 class Transaction {
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-  title: string;
+    @Column()
+    title: string;
 
-  type: 'income' | 'outcome';
+    @Column()
+    type: 'income' | 'outcome';
 
-  value: number;
+    @Column()
+    value: number;
 
-  category_id: string;
+    @Column()
+    category_id: string;
 
-  created_at: Date;
+    @ManyToOne(() => Category)
+    @JoinColumn({ name: 'category_id' })
+    category: Category;
 
-  updated_at: Date;
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Transaction;
